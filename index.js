4724046152b59dde3d5e314a1e4514481c89a883
Prince Ghoda <140962052+Prince61141@users.noreply.github.com>
2024-02-24 18:56:53 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,105 @@
+function myMenuFunction() {
+  var menuBtn = document.getElementById("myNavMenu");
+
+  if (menuBtn.className === "nav-menu") {
+    menuBtn.className += " responsive";
+  } else {
+    menuBtn.className = "nav-menu";
+  }
+}
+
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+window.onscroll = function () { headerShadow() };
+
+function headerShadow() {
+  const navHeader = document.getElementById("header");
+  const scrollicon = document.getElementById("Scroll-up");
+  const theme = document.getElementById("Theme-change");
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    navHeader.style.height = "70px";
+    navHeader.style.lineHeight = "70px";
+    scrollicon.style.display = "block";
+    theme.style.marginTop = "13px";
+  }
+  else {
+    navHeader.style.height = "90px";
+    navHeader.style.lineHeight = "90px";
+    scrollicon.style.display = "none";
+    theme.style.marginTop = "22px";
+  }
+}
+
+
+/* SCROLL REVEAL */
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+/* -- HOME -- */
+sr.reveal('.featured-text-card', {})
+sr.reveal('.featured-name', { delay: 100 })
+sr.reveal('.featured-text-info', { delay: 200 })
+sr.reveal('.featured-text-btn', { delay: 200 })
+sr.reveal('.social-media', { origin: 'left', delay: 200 })
+
+
+/* -- PROJECT -- */
+sr.reveal('.project-box', { interval: 100 })
+
+/* -- HEADINGS -- */
+sr.reveal('.top-header', {})
+sr.reveal('.education-card-left', { delay: 200 })
+
+sr.reveal('.skills-header', {})
+
+/* ----- SCROLL REVEAL LEFT_RIGHT ANIMATION ---- */
+
+/* -- CONTACT INFO -- */
+const srLeft = ScrollReveal({
+  origin: 'left',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srLeft.reveal('.about-info', { delay: 100 })
+srLeft.reveal('.education-card-right', { delay: 200 })
+srLeft.reveal('.contact-info', { delay: 100 })
+
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
+  origin: 'right',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+srRight.reveal('.skills-box', { delay: 100 })
+srRight.reveal('.form-control', { delay: 100 })
+
+
+
+/* ----- CHANGE ACTIVE LINK ----- */
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+  const scrollY = window.scrollY;
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute('id')
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+    }
+    else {
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
